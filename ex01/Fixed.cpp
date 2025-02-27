959878cff82cab32d33349f11ffb96fd3f56ca8d
david <david.david@david.com>
2025-02-27 11:34:43 +0100

    added ex01

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,89 @@
+#include "Fixed.hpp"
+
+const int Fixed::fract_bits = 8;
+
+Fixed::Fixed() : value(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+/* 
+2^x = 1 << x
+2^x is power of 2 meaning always a 1 followed by 0 in binary
+when you shift 1 by x it becomes a representation of the power of 2
+1 << 0 -> 00000001 = 1
+1 << 3 -> 00001000 = 8
+*/
+Fixed::Fixed(const int n)
+{
+    this->value = n << fract_bits;
+    std::cout << "Int constructor called" << std::endl;
+}
+
+/*
+ roundf rounds to x to the nearest int
+ same bit shifting operation as before
+*/
+Fixed::Fixed(const float n)
+{
+     this->value = roundf(n * (1 << fract_bits));
+     std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& f) : value(f.value)
+{
+    std::cout << "Copy constructor called" << std::endl;
+}
+
+// returns a reference to the current object
+// operator= is the overloaded assignment operator
+Fixed& Fixed::operator=(const Fixed& other)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if(this != &other) // need incase of self assignment (a = a)
+    {
+        this->value = other.getRawBits();
+    }
+    return (*this); // returns the current object by reference
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+void    Fixed::setRawBits(int const raw)
+{
+    value = raw;
+}
+
+int Fixed::getRawBits( void ) const
+{
+    return (value);
+}
+
+int Fixed::toInt( void ) const
+{
+    return (value >> fract_bits);
+}
+
+/* 
+    need to typecast to float to be able to retrive decimal values
+    static_cast is safer then (float)
+    shift bits to left to properly convert back 1 << 8 = 256
+    divide to get the orignal value
+ */
+float Fixed::toFloat( void ) const
+{
+     return (static_cast<float>(value) / (1 << fract_bits));
+}
+
+/* 
+    goal: tell compiler how do handle this - std::cout << fixed.value;
+    inserts converted float-value into output stream
+ */
+std::ostream& operator<<(std::ostream& os, const Fixed& value)
+{
+    os << value.toFloat();
+    return (os);
+}
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,42 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+    private:
+        int value;
+        static const int fract_bits;
+    public:
+        Fixed();
+        Fixed(const int n);
+        Fixed(const float n);
+        Fixed(const Fixed& f);
+        Fixed& operator=(const Fixed& other);
+        ~Fixed();
+        int     getRawBits( void ) const;
+        void    setRawBits(int const raw);
+        float   toFloat( void ) const;
+        int     toInt( void ) const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& value);
+
+// copy constructor
+// there is a default copy constructor
+// copy constructed is called when object is created from an existing object
+
+// copy assignment operator
+// called When an existing object is assigned the value of another existing object
+
+//fract bits
+// in fixed-point system a number is scaled by 2^fract_bits
+// when displaying the values it is divided by 2^fract_bits to retrieve the actual num
+// 2^8 = 256
+// 3.25 x 256 = 832
+// -> fixed precision (no floating point errors)
+// -> speed (less complex therefore faster)
+// -> memory efficient (uses ints which are easier to store and manipulate than floats)
+#endif
